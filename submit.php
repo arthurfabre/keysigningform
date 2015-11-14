334a0dfca7cb6eaf4f0319041ac519817fafa05d
Arthur Fabre <arthur@arthurfabre.com>
2015-11-14 23:41:41 +0000

    Change submit.php around
    Got rid of all the html, and we now read the key from $_POST

--- submit.php
+++ submit.php
@@ -1,53 +1,22 @@
 <?php
 
 if(!putenv("GNUPGHOME=/tmp/.gnupg/")) {
-    echo "Error putting environment";
+    echo "Error setting environment";
     die;
 }
 
-if(isset($_FILES['key'])) {
-    $data = file_get_contents($_FILES['key']['tmp_name']);
+if(isset($_POST['key'])) {
+
+    $key = $_POST['key'];
     $res = gnupg_init();
     $array = gnupg_import($res, $data);
+    
     if(isset($array['fingerprint'])) {
-?>
-<html>
-    <head><title>Your PGP key</title></head>
-        <body>
-            <table border=1>
-                <tr>
-                    <th>Key ID</th>
-                    <th>Owner</th>
-                    <th>Fingerprint</th>
-                </tr>
-<?php
-        foreach(gnupg_keyinfo($res, $array['fingerprint'])[0]['uids'] as $uid) {
-            echo "<tr>";
-            echo "<td>" . htmlentities(substr($array['fingerprint'], -16), ENT_QUOTES) . "</td>";
-            echo "<td>" . htmlentities($uid['uid'], ENT_QUOTES) . "</td>";
-            echo "<td>" . htmlentities($array['fingerprint'], ENT_QUOTES) . "</td>";
-            echo "</tr>";
-        }
-?>
-</table>
-</body>
-</html>
-<?php
+        echo "Key succesfully submitted";
         die;
     }
-    else {
-        echo "Failed to upload";
-    }
+
+} else {
+    echo "Key submission failed";
 }
 ?>
-
-<html>
-    <body>
-        <form action="index.php" method="post" enctype="multipart/form-data">
-            Select public key to upload: <br>
-            <input type="file" name="key" id="key"><br>
-            <input type="submit" value="Upload image" name="submit">
-        </form>
-    </body>
-</html>
-
